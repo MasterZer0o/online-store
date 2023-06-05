5911b4f0e9dcf9824a2bd275a4231200d7961954
MasterZer0o <kubapod1234@wp.pl>
2023-06-05 16:30:30 +0200

    final update

--- server/db/seeders/seed.ts
+++ server/db/seeders/seed.ts
@@ -1,46 +1,50 @@
-// import db from '../client'
 import fs from 'node:fs/promises'
-import { randomUUID } from 'node:crypto'
-import { Category } from '../models/Category.js'
-import { Product } from '../models/Product.js'
-import { logSuccess } from '~~/server/lib/logger'
+import { getDb } from '../../utils/getDb'
+import { logSuccess } from '../../utils/logger'
+import { categories, products } from '../schema/products'
+
+const db = getDb()
 
 async function seedCategories() {
-  const categories: { name: string; image: string;slug: string }[] = JSON.parse(await fs.readFile('data/categories.json', { encoding: 'utf-8' }))
-  for (const category of categories) {
-    await Category.upsert({
-      id: randomUUID(),
-      name: category.name,
-      image: category.image,
-      slug: category.slug
+  const categoriesContent: { name: string; type: 'main' | 'subcategory'; slug: string }[]
+    = JSON.parse(await fs.readFile('data/categories.json', { encoding: 'utf-8' }))
 
-    })
-    logSuccess('Added category: ', category.name)
-  }
+  await db.insert(categories).values(categoriesContent)
+  logSuccess('Successfully added categories.')
 }
 
 async function seedProducts() {
-  const products = JSON.parse(await fs.readFile('data/byCategory/bluzy.json', { encoding: 'utf-8' }))
+  let productsContent = JSON.parse(await fs.readFile('data/byCategory/bluzy.json', { encoding: 'utf-8' }))
 
-  for (const product of products) {
-    if (!product.price.includes('%') && !product.price.includes('Smart! okazja'))
-      await Product.upsert({
-        name: product.title,
-        product_image: product.image,
-        category_id: '8fc6183c-e154-49a9-a895-57b62dc78225',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-        discount_id: null,
-        price: product.price,
-        rating_rate: product?.rating?.rate || 0,
-        rating_count: product?.rating?.count || 0,
-        SKU: 'sku',
-      })
+  productsContent = productsContent.map((product: { name: string; image: string; price: string }) => {
+    return {
+      name: product.name,
+      image: product.image,
+      categoryId: 10,
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      price: parseFloat(`${product.price}`),
+      SKU: 'sku',
+    }
+  }) as NewProduct[]
 
-    logSuccess('Added product: ', product.title)
-  }
+  await db.insert(products).values(productsContent)
+
+  let productsContent2 = JSON.parse(await fs.readFile('data/byCategory/spodnie.json', { encoding: 'utf-8' }))
+
+  productsContent2 = productsContent2.map((product: { name: string; image: string; price: string }) => {
+    return {
+      name: product.name,
+      image: product.image,
+      categoryId: 8,
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      price: parseFloat(`${product.price}`),
+      SKU: 'sku',
+    }
+  }) as NewProduct[]
+
+  await db.insert(products).values(productsContent2)
+
+  logSuccess('Successfully added products.')
 }
 
-export const seed = {
-  seedCategories,
-  seedProducts
-}
+seedCategories()
